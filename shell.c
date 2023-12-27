a4d4522ab497d1639c2e7205242ea8dbafc7371d
zabitaguliyeva <7644@holbertonstudents.com.com>
2023-12-27 04:18:19 -0800

    update shell

--- shell.c
+++ shell.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(void)
-{
+int main(void) {
     char *buff, **arr;
     int status, i;
     pid_t pid;
 
-    while (1)
-    {
+    while (1) {
         if (isatty(STDIN_FILENO))
             printf("$ ");
         buff = _getline();
@@ -17,38 +18,37 @@ int main(void)
             break;
         arr = line_div(buff);
         free(buff);
-        if (!*arr)
-        {
+        if (!*arr) {
             free(arr);
             continue;
         }
-        if (strcmp(arr[0], "exit") == 0)
-        {
+        if (strcmp(arr[0], "exit") == 0) {
             for (i = 0; *(arr + i); i++)
                 free(*(arr + i));
             free(arr);
             break;
         }
         pid = fork();
-        if (pid == 0)
-        {
-            if (execvp(arr[0], arr) == -1)
-            {
+        if (pid == 0) {
+            if (execvp(arr[0], arr) == -1) {
                 perror("ERROR");
-                exit(2);
+                exit(2); 
             }
-        }
-        else if (pid > 0)
-        {
-            if (wait(&status) == -1)
-            {
+        } else if (pid > 0) {
+            if (wait(&status) == -1) {
                 perror("ERROR");
             }
+            if (WIFEXITED(status)) {
+                if (WEXITSTATUS(status) != 0) {
+                    exit(WEXITSTATUS(status));
+                }
+            }
+        } else {
+            perror("ERROR");
         }
-        else
+        if (pid == -1) {
             perror("ERROR");
-        if (pid == -1)
-            perror("ERROR");
+        }
         for (i = 0; *(arr + i); i++)
             free(*(arr + i));
         free(arr);
