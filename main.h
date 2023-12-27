395d5e405996fbf8b75fdc8f5240adf76052fb2d
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-27 00:46:30 -0800

    create main_h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MATH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+char **line_div(char *str);
+char *_getline(void);
+#endif
