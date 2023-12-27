7f5c7bb97671ae53bfbdb01e27dd41f0aa499339
zabitaguliyeva <7644@holbertonstudents.com.com>
2023-12-27 02:17:10 -0800

    Exit builtin was added

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - main func
  *
@@ -6,48 +7,55 @@
  */
 int main(void)
 {
-	char *buff, **arr;
-	int status, i;
-	pid_t pid;
-	
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("$ ");
-		buff = _getline();
-		if (buff == NULL)
-			break;
-		arr = line_div(buff);
-		free(buff);
-		if (!*arr)
-		{
-			free(arr);
-			continue;
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execvp(arr[0], arr) == -1)
-			{
-				perror("ERROR");
-				exit(1);
-			}
-		}
-		else if (pid > 0)
-		{
-			if (wait(&status) == -1)
-			{
-				perror("ERROR");
-			}
-		}
-		else
-			perror("ERROR");
-		if (pid == -1)
-			perror("ERROR");
-		for (i = 0; *(arr + i); i++)
-			free(*(arr + i));
-		free(arr);
-	}
-	free(buff);
-	return (0);
+    char *buff, **arr;
+    int status, i;
+    pid_t pid;
+
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("$ ");
+        buff = _getline();
+        if (buff == NULL)
+            break;
+        arr = line_div(buff);
+        free(buff);
+        if (!*arr)
+        {
+            free(arr);
+            continue;
+        }
+        if (strcmp(arr[0], "exit") == 0)
+        {
+            for (i = 0; *(arr + i); i++)
+                free(*(arr + i));
+            free(arr);
+            break;
+        }
+        pid = fork();
+        if (pid == 0)
+        {
+            if (execvp(arr[0], arr) == -1)
+            {
+                perror("ERROR");
+                exit(1);
+            }
+        }
+        else if (pid > 0)
+        {
+            if (wait(&status) == -1)
+            {
+                perror("ERROR");
+            }
+        }
+        else
+            perror("ERROR");
+        if (pid == -1)
+            perror("ERROR");
+        for (i = 0; *(arr + i); i++)
+            free(*(arr + i));
+        free(arr);
+    }
+    return (0);
 }
+
