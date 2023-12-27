1efa1699763f67bfbb4cb88a01ec7aff31aa9425
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-27 13:03:53 -0800

    status problem fixed

--- shell.c
+++ shell.c
@@ -4,55 +4,72 @@
  *
  * Return: int
  */
-
-int main(void) {
-    char *buff, **arr;
-    int status, i;
-    pid_t pid;
-
-    while (1) {
-        if (isatty(STDIN_FILENO))
-            printf("$ ");
-        buff = _getline();
-        if (buff == NULL)
-            break;
-        arr = line_div(buff);
-        free(buff);
-        if (!*arr) {
-            free(arr);
-            continue;
-        }
-        if (strcmp(arr[0], "exit") == 0) {
-            for (i = 0; *(arr + i); i++)
-                free(*(arr + i));
-            free(arr);
-            break;
-        }
-        pid = fork();
-        if (pid == 0) {
-            if (execvp(arr[0], arr) == -1) {
-                perror("ERROR");
-                exit(2); 
-            }
-        } else if (pid > 0) {
-            if (wait(&status) == -1) {
-                perror("ERROR");
-            }
-            if (WIFEXITED(status)) {
-                if (WEXITSTATUS(status) != 0) {
-                    exit(WEXITSTATUS(status)); 
-                }
-            }
-        } else {
-            perror("ERROR");
-        }
-        if (pid == -1) {
-            perror("ERROR");
-        }
-        for (i = 0; *(arr + i); i++)
-            free(*(arr + i));
-        free(arr);
-    }
-    return (0); 
+int main(void)
+{
+	char *buff, **arr;
+	int status, i, arr_freed = 0;
+	pid_t pid;
+	
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		buff = _getline();
+		if (buff == NULL)
+			break;
+		arr = line_div(buff);
+		free(buff);
+		if (!*arr)
+		{
+			free(arr);
+			continue;
+		}
+		if (strcmp(arr[0], "exit") == 0)
+		{
+			for (i = 0; *(arr + i) != NULL; i++)
+				free(*(arr + i));
+			free(arr);
+			arr_freed = 1;
+			break;
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execvp(arr[0], arr) == -1)
+			{
+				perror("ERROR");
+				exit(1);
+			}
+		}
+		else if (pid > 0)
+		{
+			if (waitpid(pid, &status, 0) == -1)
+			{
+				perror("ERROR");
+			}
+			if (WIFEXITED(status))
+			{
+				status = WEXITSTATUS(status);
+				if (status == 127)
+				{
+					exit(127);
+				}
+			}
+		}
+		else
+		{
+			perror("ERROR");
+		}
+		if (pid == -1)
+		{
+			perror("ERROR");
+		}
+		if (!arr_freed)
+		{
+			for (i = 0; *(arr + i) == NULL; i++)
+				free(*(arr + i));
+			free(arr);
+		}
+	}
+	return (0);
 }
-
